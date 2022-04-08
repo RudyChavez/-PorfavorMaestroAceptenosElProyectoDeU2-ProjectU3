3260af602d36f3be1d2d7a25e744e676842bac5d
Jarib03 <jarberto03@gmail.com>
2022-04-08 18:27:15 -0500

    mi proyecto profe

---
+++ len.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+char* encode(char* country, int len, int* points) {    
+    char* output = (char*)malloc(sizeof(char)* (len));
+    printf("input: %s, len: %d.\n", country, len);
+    for (int i = 0; i < len; i++){
+        output[1] = '*';
+    }
+
+    return output;
+}
